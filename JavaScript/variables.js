c2005a747c4cb02148f11d587f7de504e98b4523
NISHANKRATHORE <130035038+NISHANKRATHORE@users.noreply.github.com>
2025-04-22 13:13:22 +0000

    varibales and datatypes

---
+++ JavaScript/variables.js
@@ -0,0 +1,10 @@
+const accountID = 12321;
+let accountName = "Ricky";
+var accountCity = "jaipur"
+accountBalance = 45000
+
+// accountId = 32
+accountName = "Nishank"
+console.log(accountName)
+accountCity = "Agra"
+console.table([accountID,accountName,accountCity,accountBalance])
